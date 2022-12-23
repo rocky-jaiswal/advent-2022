4c16ddd7e10e755e3275055cc9411451211be570
Rocky Jaiswal <rocky.jaiswal@gmail.com>
2022-12-23 13:16:18 +0100

    Day 10 P 2

--- advent-kt/app/src/main/kotlin/de/rockyj/Day10.kt
+++ advent-kt/app/src/main/kotlin/de/rockyj/Day10.kt
@@ -1,9 +1,9 @@
 package de.rockyj
 
-private fun part1(input: List<String>) {
-    var lstIndex = 0
-
+private fun buildRegister(input: List<String>): Map<Int, Int> {
     val register = mutableMapOf<Int, Int>()
+
+    var lstIndex = 0
     register[lstIndex] = 1
 
     input.map { it.trim().split(Regex("""\s""")) }.forEach { instruction ->
@@ -21,11 +21,37 @@ private fun part1(input: List<String>) {
         }
     }
 
+    return register.toMap()
+}
+
+private fun part1(input: List<String>) {
+    val register = buildRegister(input)
+
     // println(register)
     println(listOf(20, 60, 100, 140, 180, 220).sumOf { it * (register[it - 1] ?: 0) })
+}
+
+private fun part2(input: List<String>) {
+    val register = buildRegister(input)
+    val crt = listOf(5, 39)
+
+    (0..crt.first()).forEach { row ->
+        (0..crt.last()).forEach { col ->
+            val idx = (row * 40) + col
+            val sprite = register[idx]!!
+            // print(idx)
+            if (listOf(sprite-1, sprite, sprite+1).contains(col)) {
+                print("#")
+            } else {
+                print(".")
+            }
+        }
+        println("")
+    }
 }
 
 fun main() {
     val lines = fileToArr("day10_1.txt")
     (part1(lines))
+    (part2(lines))
 }
